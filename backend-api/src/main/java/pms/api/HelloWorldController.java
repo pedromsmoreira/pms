682dd7f79563bf9690f8c033531642e165b6640e
Pedro Moreira <pedromsmoreira51@gmail.com>
2019-11-02 18:27:48 +0000

    fix: fix content type error

--- backend-api/src/main/java/pms/api/HelloWorldController.java
+++ backend-api/src/main/java/pms/api/HelloWorldController.java
@@ -1,20 +1,21 @@
 package pms.api;
 
+import org.json.JSONObject;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping(value = "/v1/hello", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class HelloWorldController {
     @GetMapping()
-    public ResponseEntity<String> get() {
+    public ResponseEntity<Message> get() {
+
         return ResponseEntity.ok()
                 .header("Custom-header", "foo")
-                .body("World");
+                .body(new Message("World"));
     }
 }
+

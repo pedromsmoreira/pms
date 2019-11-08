29c3d465d9dc6ec5d4d7f7cc4abf5bb5ebbdc4fe
Pedro Moreira <pedromsmoreira51@gmail.com>
2019-11-08 19:28:51 +0000

    ci/add gradlew build workflow docker (#17)
    * add gradlew build workflow
    
    * fix: fix run command for gradlew build
    
    fix run command path
    change java version from 8 to 11
    
    * grant right to build.gradle file
    
    * fix build.gradle file path
    
    add same path as gradlew build
    
    * remove trailing space after java 1.11
    
    * grant execution rights to gradlew
    
    grant execution right to gradlew, so it can build
    
    * change to java 11 instead of 1.11
    
    * add bootJar to build.gradle
    
    * some adjustments to gradle.yaml and build.gradle
    
    * revert cd to project
    
    * remove trailing spaces in gradle.yml
    
    * remove test task from gradle.yml
    
    find a better approach
    
    * add dockerfile and new action
    
    * fix workflow yaml validations and add better docker command
    
    * delete gradle workflow
    
    * remove extra - preceding run command

---
+++ backend-api/src/test/java/pms/api/HelloWorldControllerTest.java
@@ -0,0 +1,13 @@
+package pms.api;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldControllerTest {
+
+    @Test
+    public void bestUnitTestEver(){
+        assertTrue(true);
+    }
+}
